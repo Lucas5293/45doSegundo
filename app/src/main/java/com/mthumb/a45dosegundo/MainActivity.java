90831c03af7928a63bcfd4f39bf34083756dd01c
Bruno Andrade <silvandrade.bs@gmail.com>
2018-04-21 15:38:33 -0300

    QSEG 014 - Correção do modo design

--- app/src/main/java/com/mthumb/a45dosegundo/MainActivity.java
+++ app/src/main/java/com/mthumb/a45dosegundo/MainActivity.java
@@ -10,12 +10,22 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener/*, AdapterView.OnItemSelectedListener*/ {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -31,8 +41,15 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-    }
 
+        /*Spinner times = (Spinner) findViewById(R.id.spinner_times);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.times_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        times.setAdapter(adapter);*/
+
+
+
+    }
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -68,4 +85,31 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+
+    /*@Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        //ImageView time = (ImageView)findViewById(R.id.logotime);
+        switch(0){
+            case 0:
+                //time.setImageResource(R.mipmap.ic_palmeiras_logo);
+                break;
+            case 1:
+                //time.setImageResource(R.mipmap.ic_santos_logo);
+                break;
+
+            case 2:
+                //time.setImageResource(R.mipmap.ic_corinthians_logo);
+                break;
+
+            case 3:
+                //time.setImageResource(R.mipmap.ic_saopaulo_logo);
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }*/
 }
